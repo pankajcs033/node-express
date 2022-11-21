c254ba7391d4412fa56b8ea1691d4949b23862fc
pankaj <sainikumarpankaj786@gmail.com>
2022-11-21 17:02:50 +0530

    Serve Static Files

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const http = require('http');
 const { homedir } = require('os');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+// tells to express to look into this folder for static html will serve
+app.use(express.static(__dirname + '/public'));
 
 //next is used to call middle ware
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is an express server</h1></body></html>');
